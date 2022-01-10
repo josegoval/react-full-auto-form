da9cddf5f44093c75d23e3f300726e42c35479be
José Manuel Gómez Martínez <jmgomez@merkinsio.com>
2022-01-10 15:33:17 +0100

    feat: More ReactFullAutoFormProps included
    chore: axios peer dependency

--- src/components/ReactFullAutoForm/index.tsx
+++ src/components/ReactFullAutoForm/index.tsx
@@ -4,6 +4,7 @@ import {
   ErrorMessages,
   SuccessMessages
 } from '../../core/types/propTypes/messages'
+import { Axios } from 'axios'
 
 // TODO: set axios types for responses
 type OnSubmitFunction = ({
@@ -13,23 +14,39 @@ type OnSubmitFunction = ({
   values: any
   e: React.FormEvent
 }) => void
+type OnCancelFunction = (e: React.MouseEvent<HTMLButtonElement>) => void
+type OnResetFunction = ({
+  values,
+  e
+}: {
+  values: any
+  e: React.MouseEvent<HTMLButtonElement>
+}) => void
 
 type OnSuccessFunction = (response: any) => void
 type OnErrorFunction = (response: any, error: Error) => void
 
-type OnCancelFunction = (e: React.MouseEvent<HTMLButtonElement>) => void
+// TODO: set param and return types
+// new format to send to submit
+type FormatterFunction = (values: any) => any
 
 type ReactFullAutoFormProps = {
   fields: Fields
   method?: 'POST' | 'PUT' | 'PATCH'
   url?: string
   onSubmit?: OnSubmitFunction
+  onCancel?: OnCancelFunction
+  onReset?: OnResetFunction
   onSuccess?: OnSuccessFunction
   onError?: OnErrorFunction
+  hideResetButton?: boolean
+  hideCancelButton?: boolean
+  hideSubmitButton?: boolean
   successMessages?: SuccessMessages
   errorMessages?: ErrorMessages
+  formatter?: FormatterFunction
   submitFormat?: 'JSON' | 'multipart/form-data'
-  onCancel?: OnCancelFunction
+  axios?: Axios // customAxios
 }
 
 export default function ReactFullAutoForm(
